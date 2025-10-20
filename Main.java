1032954b5827f47aba510db5c7b9edfdcbefc5c6
Шварцкоп Татьяна ИП-513 <tanya.uct@gmail.com>
2025-10-21 02:11:39 +0700

    Добавлен метод getCarByBrand

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[]args){
+
+    public static Car[] getCarByBrand(Car[] cars, String brand) {
+        return java.util.Arrays.stream(cars)
+                .filter(car -> car.getBrand().equalsIgnoreCase(brand))
+                .toArray(Car[]::new);
+    }
+
+    public static void main(String[] args) {
+        Car[] cars = {
+            new Car("Toyota", "Corolla", 2018),
+            new Car("Honda", "Civic", 2020),
+            new Car("Toyota", "Camry", 2019)
+        };
+
+        Car[] toyotas = getCarByBrand(cars, "Toyota");
+        System.out.println("Toyota cars found: " + toyotas.length);
     }
 }
+
+
+
+
+
+
+
+
 

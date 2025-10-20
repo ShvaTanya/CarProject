b33674dad58aa273b131a68c1bf600fba1feb10d
Шварцкоп Татьяна ИП-513 <tanya.uct@gmail.com>
2025-10-21 02:31:08 +0700

    добавлены изменения

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 public class Main {
 
+    // Метод с шага 11
     public static Car[] getCarByBrand(Car[] cars, String brand) {
         return java.util.Arrays.stream(cars)
                 .filter(car -> car.getBrand().equalsIgnoreCase(brand))
                 .toArray(Car[]::new);
     }
+
+    // Метод с шага 14
+    public static Car[] getCarByBrandAndYears(Car[] cars, String brand, int years) {
+        int currentYear = java.time.Year.now().getValue();
+        return java.util.Arrays.stream(cars)
+                .filter(car -> car.getBrand().equalsIgnoreCase(brand) && (currentYear - car.getYear() > years))
+                .toArray(Car[]::new);
+    }
+
 
     public static void main(String[] args) {
         Car[] cars = {
@@ -15,14 +25,8 @@ public class Main {
 
         Car[] toyotas = getCarByBrand(cars, "Toyota");
         System.out.println("Toyota cars found: " + toyotas.length);
+
+        Car[] oldToyotas = getCarByBrandAndYears(cars, "Toyota", 3);
+        System.out.println("Toyota cars older than 3 years: " + oldToyotas.length);
     }
 }
-
-
-
-
-
-
-
-
-
